639b5b8cf48637857c3120c497c3af240fe26c61
Gagan Sandhu <gagan27sandhu@gmail.com>
2023-11-28 12:43:36 +0000

    channel and comments controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,34 @@
 class ChannelsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @channel = Channel.new
+  end
+
+  def create
+    @channel = Channel.new(channel_params)
+    @channel.user = current_user
+    @channel.save
+    # update this
+    # redirect_to channel_path(@channel), notice: "Channel created successfully"
+  end
+
+  def destroy
+    @channel = Channel.find(params[:id])
+    @channel.destroy
+  end
+
+  private
+
+  def channel_params
+    # update this
+    # params.require(:channel).permit(:name, :photo, :price, :description, :location)
+  end
+
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :set_channels, only: %i[new create]
+
   def new
+    @channels = Channels.find(params[:channels_id])
     @comments = Comments.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.channels = @channels
+    @comment.user_id = current_user.id
 
+    if @comment.save
+      # update this
+      # redirect_to goat_path(@channels.goat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    # update this
+    # redirect_to dashboard_path, status: :see_other
+  end
+
+
 end
