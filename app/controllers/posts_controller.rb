8cb29789fd986cc83115d532b94bd07640877257
Gagan Sandhu <gagan27sandhu@gmail.com>
2023-11-29 12:38:47 +0000

    views of the posts done needs styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,25 +7,22 @@ class PostsController < ApplicationController
     @post.channel = @channel
     @post.user = current_user
 
-    if @post.save
-      # get this checked
-      redirect_to channel_path(@channel)
-    else
-      render "channel/show", status: :unprocessable_entity
-    end
+    # if @post.save
+    #   # get this checked
+    #   redirect_to channel_path(@channel)
+    # else
+    #   render "channels/show", status: :unprocessable_entity
+    # end
 
     respond_to do |format|
       if @post.save
         format.html { redirect_to channel_path(@channel), notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-        format.js   # Add this line for AJAX response
+        # format.json { render json: { success: true, post_html: render_to_string(partial: 'posts/post', locals: { post: @post }) } }
       else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-        format.js   # Add this line for AJAX response
+        format.html { render "channels/show", status: :unprocessable_entity  }
+        # format.json { render json: { success: false, errors: @post.errors.full_messages } }
       end
     end
-
   end
 
   private
--- app/javascript/controllers/create_post_controller.js
+++ app/javascript/controllers/create_post_controller.js
@@ -2,12 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="create-post"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "formDiv"]
 
   connect() {
   }
 
   revealForm() {
-    this.formTarget.classList.toggle("d-none")
+    this.formDivTarget.classList.toggle("d-none")
   }
+
+  // create(event) {
+  //   event.preventDefault()
+  //   console.log(this.formTarget.action)
+
+  //   const url = this.formTarget.action
+  // }
 }
