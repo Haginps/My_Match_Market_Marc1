3b649f598b879c89fb11339691dbd75eab99f8d3
Jonas Schwegler <jonas.c.schwegler@gmail.com>
2023-11-30 11:33:27 +0000

    added tokens to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :set_default_tokens
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -9,4 +11,10 @@ class User < ApplicationRecord
   has_many :channels
   has_many :posts
   has_many :comments
+
+  private
+
+  def set_default_tokens
+    self.tokens ||= 5000  # Set tokens to 5000 if not already set
+  end
 end
