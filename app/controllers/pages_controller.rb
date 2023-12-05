766c353196807686063d3a5e8aa6f504b2830221
Marc ElHage <marchage153@outlook.com>
2023-12-05 12:51:19 +0000

    added percentage change of wallet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,19 +11,14 @@ class PagesController < ApplicationController
     if @today_token_history && @yesterday_token_history
       @token_change = @today_token_history.total_token - @yesterday_token_history.total_token
     else
-      @token_change = 0 # Default value if one of the records is missing
+      @token_change = 0
     end
 
     @today_tokens = current_user.user_histories.find_by(date: Date.today).tokens.round(2)
     @today_performance = current_user.user_histories.find_by(date: Date.today).performance.round(2)
+
+    @percentage_change = ((((@today_performance + @today_tokens) / 5000) * 100) - 100).round(2)
+
+    @price_increase = (5000 * (@percentage_change / 100)).round(2)
   end
-
-  # if @yesterday_token_history && @yesterday_token_history.total_token > 0
-  #   @percentage_change = (@token_change / @yesterday_token_history.total_token) * 100
-  # else
-  #   @percentage_change = 0
-  # end
-
-  # # Example of preparing other data for the dashboard view
-  # @recent_transactions = current_user.transactions.last(5)
 end
