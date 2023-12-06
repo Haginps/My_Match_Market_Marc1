4d4d761dfcf1ff181ec2fb13f3ae556beafe7286
Marc ElHage <marchage153@outlook.com>
2023-12-06 11:59:39 +0000

    fixed the shadow under the player cards in index page

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+
+// Connects to data-controller="typed-js"
+export default class extends Controller {
+  connect() {
+    };
+  }
