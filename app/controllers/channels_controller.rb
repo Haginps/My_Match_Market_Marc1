1502b9acc3f2dcbf2700e6cd01dab9d10da6fb4d
Leonard Kreutzberg <leonard.kreutzberg@icloud.com>
2023-12-07 09:46:29 +0000

    Adjusted the navbar

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,5 +1,7 @@
 class ChannelsController < ApplicationController
   def index
+    @today_tokens = current_user.user_histories.find_by(date: Date.today).tokens.round(2)
+
     @channels = Channel.all.order(created_at: :desc)
 
     @channel = Channel.new
@@ -7,6 +9,8 @@ class ChannelsController < ApplicationController
 
 
   def show
+    @today_tokens = current_user.user_histories.find_by(date: Date.today).tokens.round(2)
+
     # @channel = Channel.find(params[:id])
     # @comment = Comment.new
     @post = Post.new
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,11 +1,15 @@
 class NewsController < ApplicationController
   def index
+    @today_tokens = current_user.user_histories.find_by(date: Date.today).tokens.round(2)
+
     @news = NewsService.fetch_football_news
 
     # raise
   end
 
   def show
+    @today_tokens = current_user.user_histories.find_by(date: Date.today).tokens.round(2)
+
     @article = NewsService.fetch_football_news[params[:id].to_i]
   end
 end
