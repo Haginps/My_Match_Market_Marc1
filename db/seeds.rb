995915fd1528767c6ffddf0c8af6e62be0d17786
Jonas Schwegler <jonas.c.schwegler@gmail.com>
2024-01-31 17:47:59 +0000

    fixing the cards on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 require "open-uri"
 require "date"
 
@@ -70,40 +60,40 @@ post_2.save
 
 
 
-james_investment = Investment.new(name: "Reece James", abbreviation: "REJS", rating: rand(0..10), category: "player", description: "The best RB in the premier league", image:"https://img.chelseafc.com/image/upload/f_auto,h_390,q_90/editorial/people/first-team/2023-24/Reece_James_profile_23-24_with_sponsor_headshot-removebg.png")
+james_investment = Investment.new(name: "Reece James", abbreviation: "REJS", rating: rand(0..10), category: "player", description: "The best RB in the premier league", image:"https://img.chelseafc.com/image/upload/f_auto,h_390,q_90/editorial/people/first-team/2023-24/Reece_James_profile_23-24_with_sponsor_headshot-removebg.png", team_logo_image: "https://static-00.iconduck.com/assets.00/chelsea-icon-2048x2048-f7v75m3m.png", country_flag_image: "https://cdn-icons-png.flaticon.com/512/197/197485.png")
 james_investment.save
 
-trent_investment = Investment.new(name: "Trent AA", abbreviation: "TRAA", rating: rand(0..10), category: "player", description: "The 2nd best RB in the premier league", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p169187.png")
+trent_investment = Investment.new(name: "Trent AA", abbreviation: "TRAA", rating: rand(0..10), category: "player", description: "The 2nd best RB in the premier league", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p169187.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/hif/b/bd/Liverpool_FC.png", country_flag_image: "https://cdn-icons-png.flaticon.com/512/197/197485.png")
 trent_investment.save
 
-palmer_investment = Investment.new(name: "Cole Palmer", abbreviation: "CPLM", rating: rand(0..10), category: "player", description: "Breakthrough Chelsea player", image:"https://imagecache.365scores.com/image/upload/f_png,w_250,h_250,c_limit,q_auto:eco,d_Athletes:default.png,r_max,c_thumb,g_face,z_0.65/Athletes/78535")
+palmer_investment = Investment.new(name: "Cole Palmer", abbreviation: "CPLM", rating: rand(0..10), category: "player", description: "Breakthrough Chelsea player", image:"https://imagecache.365scores.com/image/upload/f_png,w_250,h_250,c_limit,q_auto:eco,d_Athletes:default.png,r_max,c_thumb,g_face,z_0.65/Athletes/78535", team_logo_image: "https://static-00.iconduck.com/assets.00/chelsea-icon-2048x2048-f7v75m3m.png", country_flag_image: "https://cdn-icons-png.flaticon.com/512/197/197485.png")
 palmer_investment.save
 
-messi_investment = Investment.new(name: "Lionel Messi", abbreviation: "LOME", rating: rand(0..10), category: "player", description: "The goat", image:"https://images.mlssoccer.com/image/private/t_editorial_squared_6_desktop/f_png/mls-mia-prd/urj5ghqpk8k151t5oj9g.png")
+messi_investment = Investment.new(name: "Lionel Messi", abbreviation: "LOME", rating: rand(0..10), category: "player", description: "The goat", image:"https://images.mlssoccer.com/image/private/t_editorial_squared_6_desktop/f_png/mls-mia-prd/urj5ghqpk8k151t5oj9g.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/Inter_Miami_CF_logo.svg/800px-Inter_Miami_CF_logo.svg.png", country_flag_image: "https://cdn.countryflags.com/thumbs/argentina/flag-round-250.png")
 messi_investment.save
 
-gordon_investment = Investment.new(name: "Anthony Gordon", abbreviation: "ANGR", rating: rand(0..10), category: "player", description: "The everton boy who went to Newcastle", image:"https://www.nufc.co.uk/media/79576/anthony-gordon.png?anchor=center&mode=crop&width=350&height=460&quality=75")
+gordon_investment = Investment.new(name: "Anthony Gordon", abbreviation: "ANGR", rating: rand(0..10), category: "player", description: "The everton boy who went to Newcastle", image:"https://www.nufc.co.uk/media/79576/anthony-gordon.png?anchor=center&mode=crop&width=350&height=460&quality=75", team_logo_image: "https://assets.stickpng.com/images/580b57fcd9996e24bc43c4ec.png", country_flag_image: "https://cdn-icons-png.flaticon.com/512/197/197485.png")
 gordon_investment.save
 
-bowen_investment = Investment.new(name: "Jarrod Bowen", abbreviation: "JRBW", rating: rand(0..10), category: "player", description: "England forward with rapid pace", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p178186.png")
+bowen_investment = Investment.new(name: "Jarrod Bowen", abbreviation: "JRBW", rating: rand(0..10), category: "player", description: "England forward with rapid pace", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p178186.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/sco/thumb/c/c2/West_Ham_United_FC_logo.svg/1200px-West_Ham_United_FC_logo.svg.png", country_flag_image: "https://cdn-icons-png.flaticon.com/512/197/197485.png")
 bowen_investment.save
 
-cunha_investment = Investment.new(name: "Matheus Cunha", abbreviation: "MTCH", rating: rand(0..10), category: "player", description: "Brazialin who plays at wolves", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p430871.png")
+cunha_investment = Investment.new(name: "Matheus Cunha", abbreviation: "MTCH", rating: rand(0..10), category: "player", description: "Brazialin who plays at wolves", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p430871.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/sco/thumb/f/fc/Wolverhampton_Wanderers.svg/1200px-Wolverhampton_Wanderers.svg.png", country_flag_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Brazilian_flag_icon_round.svg/1200px-Brazilian_flag_icon_round.svg.png")
 cunha_investment.save
 
-olise_investment = Investment.new(name: "Michael Olise", abbreviation: "MHOL", rating: rand(0..10), category: "player", description: "French talent", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p443661.png")
+olise_investment = Investment.new(name: "Michael Olise", abbreviation: "MHOL", rating: rand(0..10), category: "player", description: "French talent", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p443661.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/hif/c/c1/Crystal_Palace_FC_logo.png", country_flag_image: "https://cdn-icons-png.flaticon.com/512/197/197485.png")
 olise_investment.save
 
-willian_investment = Investment.new(name: "Willian", abbreviation: "WILL", rating: rand(0..10), category: "player", description: "Ex chelsea giant", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p47431.png")
+willian_investment = Investment.new(name: "Willian", abbreviation: "WILL", rating: rand(0..10), category: "player", description: "Ex chelsea giant", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p47431.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Fulham_FC_%28shield%29.svg/1200px-Fulham_FC_%28shield%29.svg.png", country_flag_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Brazilian_flag_icon_round.svg/1200px-Brazilian_flag_icon_round.svg.png")
 willian_investment.save
 
-elanga_investment = Investment.new(name: "Anthony Elanga", abbreviation: "ANEL", rating: rand(0..10), category: "player", description: "Manchest united talent on loan", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p449434.png")
+elanga_investment = Investment.new(name: "Anthony Elanga", abbreviation: "ANEL", rating: rand(0..10), category: "player", description: "Manchest united talent on loan", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p449434.png", team_logo_image: "https://cdn.freebiesupply.com/logos/large/2x/nottingham-forest-fc-logo-png-transparent.png", country_flag_image: "https://static.vecteezy.com/system/resources/previews/022/109/528/original/sweden-flag-round-shape-free-png.png")
 elanga_investment.save
 
-nunez_investment = Investment.new(name: "Darwin Nunez", abbreviation: "DRNZ", rating: rand(0..10), category: "player", description: "Uruguay stricker for liverpool", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p447203.png")
+nunez_investment = Investment.new(name: "Darwin Nunez", abbreviation: "DRNZ", rating: rand(0..10), category: "player", description: "Uruguay stricker for liverpool", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p447203.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/hif/b/bd/Liverpool_FC.png", country_flag_image: "https://flagdownload.com/wp-content/uploads/Flag_of_Uruguay_Flat_Round-1024x1024.png")
 nunez_investment.save
 
-haaland_investment = Investment.new(name: "Erling Haaland", abbreviation: "ERHL", rating: rand(0..10), category: "player", description: "The norweigian beast", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p223094.png")
+haaland_investment = Investment.new(name: "Erling Haaland", abbreviation: "ERHL", rating: rand(0..10), category: "player", description: "The norweigian beast", image:"https://resources.premierleague.com/premierleague/photos/players/110x140/p223094.png", team_logo_image: "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png", country_flag_image: "https://flagdownload.com/wp-content/uploads/Flag_of_Norway_Flat_Round.png")
 haaland_investment.save
 
 number_of_days = 30
@@ -118,17 +108,17 @@ investments = [
 
 # Initialize starting prices and momentum for each investment
 starting_prices = {
-  james_investment => 25,
+  james_investment => 20,
   trent_investment => 30,
   palmer_investment => 7,
-  messi_investment => 130,
-  haaland_investment => 90,
-  gordon_investment => 10,
-  olise_investment => 15,
+  messi_investment => 110,
+  haaland_investment => 70,
+  gordon_investment => 11,
+  olise_investment => 13,
   elanga_investment => 7,
-  bowen_investment => 8,
+  bowen_investment => 10,
   cunha_investment => 9,
-  willian_investment => 40,
+  willian_investment => 15,
   nunez_investment => 18
 }
 
@@ -171,21 +161,6 @@ momentum = {
   end
 end
 
-# number_of_days = 30
-# start_date = Date.today - number_of_days
-
-# (start_date..Date.today).each do |date|
-#   # Your logic for creating or updating historical data for each investment
-#   # Example: Update or create history for each investment
-#   investments.each do |investment|
-#     # Your logic to calculate the price or other attributes for the investment
-#     calculated_price = # ... your logic ...
-
-#     # Create or update the historical record
-#     History.create_or_find_by(date: date, investment: investment, price: calculated_price)
-#   end
-# end
-
 puts "Investments created"
 
 james = Player.new(investment: james_investment, position: "Right Back", club: "Chelsea", height: 180, weight: 80, dob: Date.parse("2000-02-20"), nationality: "England", games_played: 279, goals: 20, assists: 38, pace: 80, shooting: 72, passing: 83, dribbling: 82, defensive: 82, physics: 81)
@@ -214,12 +189,6 @@ nunez = Player.new(investment: nunez_investment, position: "Centre-Forward", clu
 nunez.save
 puts "Players created"
 
-
-
-# Holding.new(user: User.first, investment: Investment.first, purchased_price: Investment.first.histories[Investment.first.histories.length - 1].price, purchased_date: Date.yesterday, shares_amount: 10, trade: "buy")
-# u -= User.first.tokens - Holding.first.purchased_price * Holding.first.shares_amount
-# u.save
-# Seed the user history for the first user with 2 purchases
 nov_30 = Investment.last.histories[Investment.first.histories.length - 5]
 dec_02 = Investment.first.histories[Investment.first.histories.length - 3]
 
@@ -258,15 +227,5 @@ dates.each do |date|
 
   UserHistory.create(user: user_1, date: date, performance: performance, tokens: tokens)
 end
-
-# On 30 Nov, user bought the Haaland
-# user_history_2 = UserHistory.find_by(user: user_1, date: nov_30.date)
-# user_history_2.performance += holding_2.purchased_price * holding_2.shares_amount
-# user_history_2.save
-
-# # On 02 Dec, user bought the James
-# user_history_1 = UserHistory.find_by(user: user_1, date: dec_02.date)
-# user_history_1.performance += holding_1.purchased_price * holding_1.shares_amount
-# user_history_1.save
 
 puts "Seeding completed"
